737429adb753eedd9ce6ee8e293ec7c8a484df5c
Mfon <67243684+Mfonn@users.noreply.github.com>
2021-05-01 12:44:13 +0100

    Add files via upload

---
+++ js/newchange.js
@@ -0,0 +1,19 @@
+//to make the navbar sticky
+// When the user scrolls the page, execute myFunction
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 70) {
+            $(".navbar").css("backgroundColor", "#032039");
+        } else {
+            $(".navbar").css("backgroundColor", "transparent");
+        }
+    })
+})
+
+
+//for responsive datatables 
+
+
